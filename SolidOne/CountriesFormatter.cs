2e3cb8a176a74f38713c4ca51732472cd7a0d72b
sacreutz <33383005+sacreutz@users.noreply.github.com>
2019-04-03 16:52:59 -0500

    first

---
+++ SolidOne/CountriesFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SRPExample
+{
+    public static class CountriesFormatter
+    {
+        public static string FormatForConsole(Country country)
+        {
+            return string.Format("{0}, Capital: {1} - Region: {2}", country.Name, country.Capital, country.Region);
+        }
+    }
+}
